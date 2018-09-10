a45b79aa07d53356e8cd7abcbfef21d14db4cc43
KnutP <apknut@gmail.com>
2018-09-09 22:13:17 -0400

    Final code from Ri30H 2018.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,288 +1,85 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
-import static java.lang.Thread.sleep;
 
 public class Robot {
 
-    MecanumDrivetrain drivetrain;
+    private DcMotor lDriveM, rDriveM, lLift, rLift, extension, intake, pivot;
+    private Servo hopper;
 
-    private HardwareMap hwMap =  null;
+    private HardwareMap hwMap;
+
     OpMode opMode;
-    private DcMotor lIntake, rIntake, lift, relic;
-    private Servo hopper, gripper, jewelPivot, jewelArm, relicGripper, relicArm;
-
-    private ColorSensor colorSensor;
-
-    private ClosableVuforiaLocalizer vuforia;
-    private ElapsedTime vutimer = new ElapsedTime();
-    String vuforiaLicenseKey = "Ae3H91v/////AAAAGT+4TPU5r02VnQxesioVLr0qQzNtgdYskxP7aL6/" +
-            "yt9VozCBUcQrSjwec5opfpOWEuc55kDXNNSRJjLAnjGPeaku9j4nOfe7tWxio/xj/uNdPX7fEHD0j5b" +
-            "5M1OgX/bkWoUV6pUTAsKj4GaaAKIf76vnX36boqJ7BaMJNuhkYhoQJWdVqwFOC4veNcABzJRw4mQmfO" +
-            "3dfPvNVjxDl8kgdBEQOZRi9kFDy9w3cTLatSGZne3IvyaYYd8uckzPnQb5Mgel3ORjar/84qO+GBmG2" +
-            "vDhmiv+vkY4gbCtS0em5LM+7CIMuZa5vO9GmtqXyNsoCp9zpPlgZHc1OJ7javiI5jAzWEKCPjZcmLAkSs7k+amw";
 
 
-    public void init(HardwareMap ahwMap, LinearOpMode linOp) {
-        // Save reference to Hardware map
-        hwMap = ahwMap;
-        opMode = linOp;
-
-        lIntake = hwMap.get(DcMotor.class, "LI");
-        rIntake = hwMap.get(DcMotor.class, "RI");
-        lift = hwMap.get(DcMotor.class, "lift");
-        relic = hwMap.get(DcMotor.class, "relic");
-
-        hopper = hwMap.get(Servo.class, "hopper");
-        gripper = hwMap.get(Servo.class, "gripper");
-        jewelPivot = hwMap.get(Servo.class, "jewelPivot");
-        jewelArm = hwMap.get(Servo.class, "jewelArm");
-        relicGripper = hwMap.get(Servo.class, "rGripper");
-        relicArm = hwMap.get(Servo.class, "rArm");
-
-        colorSensor = hwMap.get(ColorSensor.class, "sensor_color");
-
-        hopper.setPosition(0.33);
-        gripper.setPosition(0.65);
-        jewelPivot.setPosition(0.5);
-        jewelArm.setPosition(0.78);
-        relicGripper.setPosition(0.6);
-        relicArm.setPosition(1);
-
-        drivetrain = new MecanumDrivetrain(this);
-
-        drivetrain.init((ahwMap));
-    }
-
-    /* Initialize standard Hardware interfaces */
     public void init(HardwareMap ahwMap, OpMode op) {
         // Save reference to Hardware map
         hwMap = ahwMap;
         opMode = op;
 
-        lIntake = hwMap.get(DcMotor.class, "LI");
-        rIntake = hwMap.get(DcMotor.class, "RI");
-        lift = hwMap.get(DcMotor.class, "lift");
-        relic = hwMap.get(DcMotor.class, "relic");
+        this.hopper = hwMap.get(Servo.class, "hopper");
 
-        hopper = hwMap.get(Servo.class, "hopper");
-        gripper = hwMap.get(Servo.class, "gripper");
-        jewelPivot = hwMap.get(Servo.class, "jewelPivot");
-        jewelArm = hwMap.get(Servo.class, "jewelArm");
-        relicGripper = hwMap.get(Servo.class, "rGripper");
-        relicArm = hwMap.get(Servo.class, "rArm");
+        // Define and Initialize Motors
+        this.lLift = hwMap.get(DcMotor.class, "lLift");
+        this.rLift = hwMap.get(DcMotor.class, "rLift");
+        this.extension = hwMap.get(DcMotor.class, "extension");
+        this.intake = hwMap.get(DcMotor.class, "intake");
+        this.pivot = hwMap.get(DcMotor.class, "pivot");
 
-        hopper.setPosition(0.33);
-        gripper.setPosition(0.65);
-        jewelPivot.setPosition(0.5);
-        jewelArm.setPosition(0.98);
-        relicGripper.setPosition(0.6);
-        relicArm.setPosition(1);
+        this.lDriveM  = hwMap.get(DcMotor.class, "LD");
+        this.rDriveM = hwMap.get(DcMotor.class, "RD");
+        lDriveM.setDirection(DcMotor.Direction.REVERSE);
+        rDriveM.setDirection(DcMotor.Direction.FORWARD);
 
-        drivetrain = new MecanumDrivetrain(this);
+        stop();
 
-        drivetrain.init((ahwMap));
-    }
+        setMotorMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setMotorMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-
-    // Moves the drive train using the given x, y, and rotational velocities
-    public void drive(double xVelocity, double yVelocity, double wVelocity){
-        drivetrain.drive(xVelocity, yVelocity, wVelocity);
-    }
-
-    public void encoderDrive(double yDist, double maxSpeed){
-        drivetrain.encoderDrive(yDist, maxSpeed);
-    }
-
-    public void gyroTurn(int wDist, String direction){
-        drivetrain.gyroTurn(wDist, direction);
-    }
-
-    public void runIntake(double power){
-        lIntake.setPower(-power);
-        rIntake.setPower(power);
-    }
-
-    public void runLift(double power){
-        lift.setPower(power);
-    }
-
-    public void runRelic(double power){
-        relic.setPower(power);
-    }
-
-    public void setHopperPosition(double position){
-        hopper.setPosition(position);
-    }
-
-    public void setGripperPosition(double position){
-        gripper.setPosition(position);
-    }
-
-    public void setJewelPivotPosition(double position){
-        jewelPivot.setPosition(position);
-    }
-
-    public void setJewelArmPosition(double position){
-        jewelArm.setPosition(position);
-    }
-
-    public void setRelicGripperPosition(double position){
-        relicGripper.setPosition(position);
-    }
-
-    public void setRelicArmPosition(double position){
-        relicArm.setPosition(position);
-    }
-
-    public void bumpJewel(String alliance){
-
-        jewelArm.setPosition(0.8);
-        try{
-            sleep(300);
-        }catch(InterruptedException e){
-            opMode.telemetry.addLine("I broke from sleep. :( (bumpJewel function)");
-        }
-
-        jewelPivot.setPosition(0.7);
-        try{
-            sleep(300);
-        }catch(InterruptedException e){
-            opMode.telemetry.addLine("I broke from sleep. :( (bumpJewel function)");
-        }
-        jewelArm.setPosition(0.32);
-        try{
-            sleep(500);
-        }catch(InterruptedException e){
-            opMode.telemetry.addLine("I broke from sleep. :( (bumpJewel function)");
-        }
-        jewelPivot.setPosition(0.6);
-        try{
-            sleep(300);
-        }catch(InterruptedException e){
-            opMode.telemetry.addLine("I broke from sleep. :( (bumpJewel function)");
-        }
-        jewelArm.setPosition(0.27);
-
-
-        float[] hsvValues = new float[3];
-        final float values[] = hsvValues;
-
-        colorSensor.enableLed(true);
-        opMode.telemetry.addLine("raw Android color: ")
-                .addData("a", "%02x", colorSensor.alpha())
-                .addData("r", "%02x", colorSensor.red())
-                .addData("g", "%02x", colorSensor.green())
-                .addData("b", "%02x", colorSensor.blue());
-        opMode.telemetry.update();
-
-        try{
-            sleep(500);
-        }catch(InterruptedException e){
-            opMode.telemetry.addLine("I broke from sleep. :( (bumpJewel function)");
-            opMode.telemetry.update();
-        }
-
-
-        if(alliance.equals("blue")){
-            if(colorSensor.red() > colorSensor.blue()){
-                jewelPivot.setPosition(0.8);
-            }
-            else if(colorSensor.red() < colorSensor.blue()){
-                jewelPivot.setPosition(0.4);
-            }
-        }
-        else if(alliance.equals("red")){
-            if(colorSensor.red() > colorSensor.blue()){
-                jewelPivot.setPosition(0.4);
-            }
-            else if(colorSensor.red() < colorSensor.blue()){
-                jewelPivot.setPosition(0.8);
-            }
-        }
-
-        try{
-            sleep(500);
-        }catch(InterruptedException e){
-            opMode.telemetry.addLine("I broke from sleep. :( (bumpJewel function)");
-            opMode.telemetry.update();
-        }
-
-        jewelArm.setPosition(0.8);
-        jewelPivot.setPosition(0.5);
+        lDriveM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rDriveM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 
     }
 
+    public void setIntakePower(double intakePower){
+        intake.setPower(intakePower);
+    }
 
-    public String activateVuforia () {
-        int cameraMonitorViewId = hwMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hwMap.appContext.getPackageName()); // Get the camera!
-        VuforiaLocalizer.Parameters parameters_Vuf = new VuforiaLocalizer.Parameters(cameraMonitorViewId);  // Prepare the parameters
-        parameters_Vuf.vuforiaLicenseKey = vuforiaLicenseKey;
-        parameters_Vuf.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;  // Look through the inward facing camera
-        this.vuforia = new ClosableVuforiaLocalizer(parameters_Vuf);             // Apply Parameters
+    public void setLiftPower(double liftPower){
+        lLift.setPower(liftPower);
+        rLift.setPower(-liftPower);
+    }
 
-        VuforiaTrackables Cryptokey = this.vuforia.loadTrackablesFromAsset("RelicVuMark");  // Create VuMarks from Pictograph
-        VuforiaTrackable Targets = Cryptokey.get(0);
-        Targets.setName("Targets");
+    public void setExtensionPower(double extensionPower){
+        extension.setPower(extensionPower);
+    }
 
-        Cryptokey.activate();
-        opMode.telemetry.addData("Searching for Key", "...");
-        opMode.telemetry.update();
-        String vufkey = decryptKey(Targets);
-        opMode.telemetry.addData("Key Found", vufkey);
-        opMode.telemetry.update();
+    public void setPivotPower(double pivotPower){
+        pivot.setPower(pivotPower);
+    }
 
-        this.vuforia.close();
-        return vufkey;
+    public void setHopperPosition(double hopperPosition){
+        hopper.setPosition(hopperPosition);
+    }
+
+    public void drive(double lPower, double rPower){
+        lDriveM.setPower(lPower);
+        rDriveM.setPower(rPower);
     }
 
 
-    private String decryptKey(VuforiaTrackable cryptokeys) {
-
-        String key = "";
-        vutimer.reset();
-        RelicRecoveryVuMark vuMark;
-        while (((LinearOpMode) opMode).isStarted()) {
-            vuMark = RelicRecoveryVuMark.from(cryptokeys);
-
-            if (vuMark == RelicRecoveryVuMark.LEFT) {       //Store which cryptokey is found and
-                key = "KeyLeft";            //update the telemetry accordingly.
-                opMode.telemetry.addData("Spotted Key", "Left!");
-                opMode.telemetry.update();
-            } else if (vuMark == RelicRecoveryVuMark.CENTER) {
-                key = "KeyCenter";
-                opMode.telemetry.addData("Spotted Key", "Center!");
-                opMode.telemetry.update();
-            } else if (vuMark == RelicRecoveryVuMark.RIGHT) {
-                key = "KeyRight";
-                opMode.telemetry.addData("Spotted Key", "Right!");
-                opMode.telemetry.update();
-            } else {
-                key = "KeyUnknown";
-                opMode.telemetry.addData("Spotted Key", "Unknown");
-                opMode.telemetry.update();
-            }
-            if(((LinearOpMode) opMode).isStopRequested() || key != "KeyUnknown" || vutimer.time() > 3) {
-                break;           //Stop this code if a stop is requested, or if the VuMark has been
-            }                    //found, or if more than 3 seconds have passed.
-        }
-        return key;
+    public void stop(){
+        lDriveM.setPower(0);
+        rDriveM.setPower(0);
     }
 
-
-
-
-
+    public void setMotorMode(DcMotor.RunMode mode) {
+        rDriveM.setMode(mode);
+        lDriveM.setMode(mode);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -6,17 +6,19 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name = "CRITeleop", group = "Iterative Opmode")
+@TeleOp(name = "Ri30HTeleop", group = "Iterative Opmode")
 //@Disabled
 public class Teleop extends OpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private Robot robot = new Robot();
-    private enum HopperPosition {Idle, level, up}
+    private enum HopperPosition {Idle, up}
     private HopperPosition hopperPosition = HopperPosition.Idle;
-    private int gripper = 1;
+
     private boolean bPressedBefore = false;
+    private boolean intakeOn= false;
+    private double intakePower = 0;
 
 
     @Override
@@ -35,8 +37,6 @@ public class Teleop extends OpMode {
 
     @Override
     public void start() {
-        robot.setJewelPivotPosition(0.5);
-        robot.setJewelArmPosition(0.78);
         runtime.reset();
     }
 
@@ -44,64 +44,30 @@ public class Teleop extends OpMode {
     public void loop() {
 
         // DRIVER CODE
-        double xVelocity;
-        double yVelocity;
-        double wVelocity;
+        double lPower;
+        double rPower;
 
-        yVelocity = gamepad1.left_stick_y*Math.abs(gamepad1.left_stick_y);
-        xVelocity = gamepad1.left_stick_x*Math.abs(gamepad1.left_stick_x);
-        wVelocity = gamepad1.right_stick_x*Math.abs(gamepad1.right_stick_x)/2;
-        yVelocity = Range.clip(yVelocity, -1.0, 1.0);
-        xVelocity = Range.clip(xVelocity, -1.0, 1.0);
-        wVelocity = Range.clip(wVelocity, -1.0, 1.0);
+        lPower = gamepad1.left_stick_y*Math.abs(gamepad1.left_stick_y);
+        rPower = gamepad1.right_stick_y*Math.abs(gamepad1.right_stick_y);
+        lPower = Range.clip(lPower, -1.0, 1.0);
+        rPower = Range.clip(rPower, -1.0, 1.0);
 
-        robot.drive(xVelocity, yVelocity, wVelocity);
-
-        if(gamepad1.b){
-            robot.setJewelArmPosition(0.78);
-        }
+        robot.drive(lPower, rPower);
 
 
         // OPERATOR CODE
 
-        // Glyph intake
-        robot.runIntake(gamepad2.right_stick_y);
-
-        // Hopper lift
-        robot.runLift(gamepad2.left_stick_y);
-
-        // Hopper positions
-        if(gamepad2.a){
-            hopperPosition = HopperPosition.Idle;
-        } else if(gamepad2.x){
-            hopperPosition = HopperPosition.level;
-        } else if(gamepad2.y){
-            hopperPosition = HopperPosition.up;
-        }
-
-        switch(hopperPosition){
-            case Idle:
-                robot.setHopperPosition(0.54);
-                break;
-            case level:
-                robot.setHopperPosition(0.33);
-                break;
-            case up:
-                robot.setHopperPosition(0);
-                break;
-            default:
-                robot.setHopperPosition(0.33);
-                break;
-        }
 
         // Hopper gripper
         if(gamepad2.b && !bPressedBefore){
-            if(gripper == 1 ){
-                gripper = 2;
+            if(!intakeOn ){
+                intakeOn = true;
+                intakePower = -1;
                 bPressedBefore = true;
             }
             else{
-                gripper = 1;
+                intakeOn = false;
+                intakePower = 0;
                 bPressedBefore = true;
             }
         }
@@ -109,42 +75,53 @@ public class Teleop extends OpMode {
             bPressedBefore = false;
         }
 
-        switch(gripper){
-            case 1:
-                robot.setGripperPosition(0);
-                break;
-            case 2:
-                robot.setGripperPosition(0.6);
+        if(gamepad2.x){
+            intakePower = 1;
         }
+        robot.setIntakePower(intakePower);
 
-        // Relic extension
+
+        // Intake
+        robot.setPivotPower(gamepad2.right_stick_y);
+        // Extension
+        robot.setExtensionPower(-gamepad2.left_stick_y);
+//        // Pivot
+//        if(gamepad2.left_bumper){
+//            robot.setPivotPower(-1);
+//        }else if(gamepad2.left_trigger>0.5){
+//            robot.setPivotPower(0.5);
+//        } else{
+//            robot.setPivotPower(0);
+//        }
+
+        // Lift
         if(gamepad2.dpad_up){
-            robot.runRelic(-1);
-        }
-        else if (gamepad2.dpad_down){
-            robot.runRelic(1);
-        } else{
-            robot.runRelic(0);
-        }
-
-        // Relic arm
-        if(gamepad2.left_bumper){
-            robot.setRelicArmPosition(0.35);
-        } else if (gamepad2.left_trigger>0.5){
-            robot.setRelicArmPosition(1);
+            robot.setLiftPower(-1);
+        } else if (gamepad2.dpad_down){
+            robot.setLiftPower(1);
+        }else{
+            robot.setLiftPower(0);
         }
 
-        // Relic gripper
-        if(gamepad2.right_bumper){
-            robot.setRelicGripperPosition(0.65);
-        } else if (gamepad2.right_trigger>0.5){
-            robot.setRelicGripperPosition(1);
+        // Hopper positions
+        if(gamepad2.a){
+            hopperPosition = HopperPosition.Idle;
+        } else if(gamepad2.y){
+            hopperPosition = HopperPosition.up;
         }
 
+        switch(hopperPosition){
+            case Idle:
+                robot.setHopperPosition(1);
+                break;
+            case up:
+                robot.setHopperPosition(0.1);
+                break;
+            default:
+                robot.setHopperPosition(1);
+                break;
+        }
 
-        // Show the elapsed game time and velocities.
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "X (%.2f), Y (%.2f), W (%.2f)", xVelocity, yVelocity, wVelocity);
     }
 
     @Override
